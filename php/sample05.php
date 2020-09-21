558b51b5fc62ca3a6f94605c7a9d4dbf6039bd43
takekoshi <takekoshi.work@gmail.com>
2020-09-21 21:45:21 +0900

    変数の使用完了

--- php/sample05.php
+++ php/sample05.php
@@ -12,17 +12,17 @@
 </head>
 <body>
 <header>
-<h1 class="font-weight-normal">PHP</h1>    
+<h1 class="font-weight-normal">PHP</h1>
 </header>
 
 <main>
 <h2>Practice</h2>
 <pre>
-<?php $sum = 100+1050+200; ?>
-<?php $tax = 1.08; ?>
-合計金額は： <?php print($sum); ?>円です
-税込み価格は： <?php print($sum*$tax); ?>円です
+<?php $sum = 1520+600+200; ?>
+<?php $tax = 1.1; ?>
+合計金額は： <?php print($sum); ?>円です。
+税込み価格は： <?php print($sum*$tax); ?>円です。
 </pre>
 </main>
-</body>    
+</body>
 </html>
